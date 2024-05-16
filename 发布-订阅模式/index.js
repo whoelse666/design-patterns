9a95c78c4308de481aaea1f9b2f028144e7a9b17
whoelse <347300979@qq.com>
2024-05-16 21:32:47 +0800

    update :发布-订阅模式

--- 发布-订阅模式/index.js
+++ 发布-订阅模式/index.js
@@ -1,5 +1,52 @@
 /* 发布-订阅模式（Publish-Subscribe Pattern） */
 
+var publisher = {
+  events: {},
+
+  addEvent: function (event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+  },
+
+  removeEvent: function (event, callback) {
+    if (this.events[event]) {
+      if (callback) {
+        for (var i = 0; i < this.events[event].length; i++) {
+          if (this.events[event][i] === callback) {
+            this.events[event].splice(i, 1);
+            break;
+          }
+        }
+      } else {
+        this.events[event] = [];
+      }
+    }
+  },
+
+  publishEvent: function (event, data) {
+    if (this.events[event]) {
+      for (var i = 0; i < this.events[event].length; i++) {
+        this.events[event][i](data);
+      }
+    }
+  }
+};
+
+var subscriber = {
+  handleEvent: function (data) {
+    console.log(data);
+  }
+};
+
+publisher.addEvent("event1", subscriber.handleEvent);
+
+publisher.publishEvent("event1", "Hello, world!");
+
+publisher.removeEvent("event1", subscriber.handleEvent);
+publisher.publishEvent("event1", "Hello, world!");
+
 class MyEvent {
   constructor(name) {
     console.log("MyEvent", name);
